24cded0148f5627686a41818836d3142e2d27a0f
rhaneyko <rhaneykohonorio@gmail.com>
2024-05-23 22:20:11 -0400

    commit

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import './Login.css'
+
 const Login = () => {
   return (
-    <div>
-      TELA DE LOGIN
+    <div className='login-container'>
+      <h1>LOGIN</h1>
+      <form className='form'>
+        <label className='label-form'>Usuário</label>
+        <input placeholder='coloque seu usuário'/>
+        <label>Senha</label>
+        <input placeholder='coloque sua senha'/>
+      </form>
     </div>
   )
 }
