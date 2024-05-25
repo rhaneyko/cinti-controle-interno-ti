f998ff541d9e4946d572d55ddd733d3fc44fb565
rhaneyko <rhaneykohonorio@gmail.com>
2024-05-24 21:31:25 -0400

    commit

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -8,9 +8,10 @@ const Login = () => {
       <h1>LOGIN</h1>
       <form className='form'>
         <label className='label-form'>Usuário</label>
-        <input placeholder='coloque seu usuário'/>
-        <label>Senha</label>
-        <input placeholder='coloque sua senha'/>
+        <input className='input-form' placeholder='coloque seu usuário'/>
+        <label className='label-form'>Senha</label>
+        <input className='input-form' placeholder='coloque sua senha'/>
+        <button>Login</button>
       </form>
     </div>
   )
