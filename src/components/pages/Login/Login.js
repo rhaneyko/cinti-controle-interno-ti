2cb2b98626536c0de86068520b58a8c4a9522190
rhaneyko <rhaneykohonorio@gmail.com>
2024-05-24 21:47:50 -0400

    commit

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,17 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
 
 import './Login.css'
 
 const Login = () => {
+  const { signin } = useAuth();
+  const navigate = useNavigate;
+
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = () => {
+    if(!user || !password){
+      setError('Preencha todos os campos');
+      return;
+    }
+    const res = login(user, password);
+    if(res){
+      setError(res);
+      return
+    }
+    navigate('/home')
+  }
+
   return (
     <div className='login-container'>
-      <h1>LOGIN</h1>
+      <h1>Entre</h1>
       <form className='form'>
-        <label className='label-form'>Usuário</label>
-        <input className='input-form' placeholder='coloque seu usuário'/>
+        <label className='label-form'>E-mail</label>
+        <input 
+          className='input-form' 
+          placeholder='Digite seu e-mail'
+          type='text'
+          value={user}
+          onChange={(e) => [setUser(e.target.value), setError('')]}
+        />
         <label className='label-form'>Senha</label>
-        <input className='input-form' placeholder='coloque sua senha'/>
-        <button>Login</button>
+        <input 
+          className='input-form' 
+          placeholder='Digite sua senha'
+          type={'password'}
+          onChange={(e) => [setPassword(e.target.value), setError('')]}
+        />
+        <button>Entrar</button>
       </form>
     </div>
   )
